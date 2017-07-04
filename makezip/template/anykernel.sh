2ab4d5ef7345f1c9a109cc02d4f2bf742f22eeb3
cmulk <cjmulk2@gmail.com>
2017-07-03 23:13:27 -0500

    Add simple template and makefile to build flashable kernel zip using AnyKernel2

---
+++ makezip/template/anykernel.sh
@@ -0,0 +1,72 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=Clark-Simple-Kernel
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=clark
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+} # end properties
+
+# shell variables
+block=/dev/block/platform/soc.0/f9824900.sdhci/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+chmod -R 755 $ramdisk
+#chmod 644 $ramdisk/sbin/media_profiles.xml
+
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+backup_file init.rc;
+insert_line init.rc "init.customgov.rc" after "import /init.usb.configfs.rc" "import /init.customgov.rc";
+# end ramdisk changes
+
+# init.tuna.rc
+#backup_file init.tuna.rc;
+#insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+#append_file init.tuna.rc "dvbootscript" init.tuna;
+
+# init.superuser.rc
+#if [ -f init.superuser.rc ]; then
+#  backup_file init.superuser.rc;
+#  replace_string init.superuser.rc "Superuser su_daemon" "# su daemon" "\n# Superuser su_daemon";
+#  prepend_file init.superuser.rc "SuperSU daemonsu" init.superuser;
+#else
+#  replace_file init.superuser.rc 750 init.superuser.rc;
+#  insert_line init.rc "init.superuser.rc" after "on post-fs-data" "    import /init.superuser.rc";
+#fi;
+
+# fstab.tuna
+#backup_file fstab.tuna;
+#patch_fstab fstab.tuna /system ext4 options "nodiratime,barrier=0" "nodev,noatime,nodiratime,barrier=0,data=writeback,noauto_da_alloc,discard";
+#patch_fstab fstab.tuna /cache ext4 options "barrier=0,nomblk_io_submit" "nosuid,nodev,noatime,nodiratime,errors=panic,barrier=0,nomblk_io_submit,data=writeback,noauto_da_alloc";
+#patch_fstab fstab.tuna /data ext4 options "nomblk_io_submit,data=writeback" "nosuid,nodev,noatime,errors=panic,nomblk_io_submit,data=writeback,noauto_da_alloc";
+#append_file fstab.tuna "usbdisk" fstab;
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
